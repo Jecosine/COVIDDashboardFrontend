41d622cc5b9c77298616f7cb1a8f01d882b125d5
JecosineMoc <mj_ol@outlook.com>
2021-06-09 18:11:48 +0800

    INIT

---
+++ src/views/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="container">
+    
+  </div>
+</template>
+
+<script lang="ts">
+// import { Component, Prop } from 'vue-property-decorator'
+import Component from 'vue-class-component'
+
+@Component({
+  props: {}
+})
+export default class extends Vue {
+
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
